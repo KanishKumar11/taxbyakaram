efa7029974f79619e6b1bcd2a32f9232ecf4bd72
Kanish Kumar <kanishkumar11@outlook.in>
2025-02-02 11:09:37 +0530

    completed

--- src/app/itr-1/page.jsx
+++ src/app/itr-1/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Simula } from "@/lib/fonts";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Link from "next/link";
 
 const documents = [
   {
@@ -73,6 +74,7 @@ const documents = [
     title: "Rental Income (if applicable)",
     items: [
       "Rent receipts or details of rent received (though limited income is allowed under ITR-1).",
+      "Ensure all the details are accurate before filing your return to avoid errors or notices from the Income Tax Department.",
     ],
   },
 ];
@@ -89,26 +91,33 @@ export default function ITR1Page() {
           <p className="text-lg mt-2">
             We at TaxByAkram are here to handle it all for you.
           </p>
-          <button className="mt-4 px-6 py-3 bg-white text-[#96259C] font-bold rounded-lg">
-            Contact Us
-          </button>
+          <Link href="/https://wa.me/+919949947465">
+            <button className="mt-4 px-6 py-3 bg-white text-[#96259C] hover:bg-slate-100 font-bold rounded-lg">
+              Contact Us
+            </button>
+          </Link>
         </div>
 
         <div>
-          <h3 className="text-3xl font-bold">Documents needed to file ITR-1</h3>
-          <p className="mt-2">
+          <h3 className={`lg:text-7xl text-4xl ${Simula.className} py-10 `}>
+            Documents needed to file ITR-1
+          </h3>
+          {/* <p className="mt-2">
             Filing Income Tax Return (ITR-1) in India requires you to gather
             certain documents and details to ensure a smooth and accurate filing
             process.
-          </p>
+          </p> */}
 
           <ul className="mt-4 space-y-6">
             {documents.map((section, index) => (
-              <li key={index}>
-                <h4 className="text-xl font-bold">
-                  {index + 1}. {section.title}
+              <li
+                key={index}
+                className="flex lg:gap-5 gap-1 lg:flex-row flex-col"
+              >
+                <h4 className="text-xl flex items-center justify-center lg:min-w-[300px] max-w-[300px] bg-[#EFEFEF] py-5 lg:px-10 rounded-2xl text-balance text-center lg:font-normal font-bold">
+                  {section.title}
                 </h4>
-                <ul className="list-disc pl-6 mt-1">
+                <ul className="list-none pl-6  bg-[#EFEFEF] py-5 px-10 rounded-2xl w-full text-lg">
                   {section.items.map((item, i) => (
                     <li key={i}>{item}</li>
                   ))}
@@ -117,9 +126,11 @@ export default function ITR1Page() {
             ))}
           </ul>
         </div>
-        <button className="mt-4 px-16 py-6 bg-[#96259C] text-white font-bold rounded-lg text-3xl">
-          Contact Us
-        </button>
+        <Link href="https://wa.me/+919949947465">
+          <button className="mt-4 px-16 py-6 bg-[#96259C] hover:bg-[#96259C]/90 my-20 text-white font-bold rounded-lg text-3xl">
+            Contact Us
+          </button>
+        </Link>
       </div>
       <Footer />
     </>
--- src/app/itr-3/page.jsx
+++ src/app/itr-3/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Simula } from "@/lib/fonts";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Link from "next/link";
 
 const documents = [
   {
@@ -109,28 +110,35 @@ export default function ITR3Page() {
             Want to file ITR-3
           </h2>
           <p className="text-lg mt-2">
-            We at Taab/Akram are here to handle it all for you.
+            We at TaxByAkram are here to handle it all for you.
           </p>
-          <button className="mt-4 px-6 py-3 bg-white text-[#96259C] font-bold rounded-lg">
-            Contact Us
-          </button>
+          <Link href="/https://wa.me/+919949947465">
+            <button className="mt-4 px-6 py-3 bg-white text-[#96259C] hover:bg-slate-100 font-bold rounded-lg">
+              Contact Us
+            </button>
+          </Link>
         </div>
 
         <div>
-          <h3 className="text-3xl font-bold">Documents needed to file ITR-3</h3>
-          <p className="mt-2">
-            Filing Income Tax Return (ITR-3) in India requires you to gather
-            certain documents and details to ensure a smooth and accurate filing
-            process.
-          </p>
+          <h3 className={`lg:text-7xl text-4xl ${Simula.className} py-10 `}>
+            Documents needed to file ITR-3
+          </h3>
+          {/* <p className="mt-2">
+          Filing Income Tax Return (ITR-1) in India requires you to gather
+          certain documents and details to ensure a smooth and accurate filing
+          process.
+        </p> */}
 
           <ul className="mt-4 space-y-6">
             {documents.map((section, index) => (
-              <li key={index}>
-                <h4 className="text-xl font-bold">
-                  {index + 1}. {section.title}
+              <li
+                key={index}
+                className="flex lg:gap-5 gap-1 lg:flex-row flex-col"
+              >
+                <h4 className="text-xl flex items-center justify-center lg:min-w-[300px] max-w-[300px] bg-[#EFEFEF] py-5 lg:px-10 rounded-2xl text-balance text-center lg:font-normal font-bold">
+                  {section.title}
                 </h4>
-                <ul className="list-disc pl-6 mt-1">
+                <ul className="list-none pl-6  bg-[#EFEFEF] py-5 px-10 rounded-2xl w-full text-lg">
                   {section.items.map((item, i) => (
                     <li key={i}>{item}</li>
                   ))}
@@ -139,9 +147,11 @@ export default function ITR3Page() {
             ))}
           </ul>
         </div>
-        <button className="mt-4 px-16 py-6 bg-[#96259C] text-white font-bold rounded-lg text-3xl">
-          Contact Us
-        </button>
+        <Link href="https://wa.me/+919949947465">
+          <button className="mt-4 px-16 py-6 bg-[#96259C] hover:bg-[#96259C]/90 my-20 text-white font-bold rounded-lg text-3xl">
+            Contact Us
+          </button>
+        </Link>
       </div>
       <Footer />
     </>
